8a5a4daf4e32e91bca40d06d440168f1a82e4197
farkhutdinova <evgeniyafarhutdinova@gmail.com>
2019-10-26 21:02:58 +0200

    added menu in toolbar

--- app/src/main/java/com/commonsware/todo/MainActivity.kt
+++ app/src/main/java/com/commonsware/todo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.commonsware.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    toolbar.title = getString(R.string.app_name)
+    toolbar.inflateMenu(R.menu.actions)
   }
 }
