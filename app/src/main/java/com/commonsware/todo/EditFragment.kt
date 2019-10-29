bfb28dd80e30f452ae26ff4b86c1ffe0ceeda0e6
farkhutdinova <evg.farkhutdinova@gmail.com>
2019-10-29 17:00:45 +0100

    save item after edit

--- app/src/main/java/com/commonsware/todo/EditFragment.kt
+++ app/src/main/java/com/commonsware/todo/EditFragment.kt
@@ -1,10 +1,11 @@
 package com.commonsware.todo
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.commonsware.todo.databinding.TodoEditBinding
 
@@ -22,5 +23,52 @@ class EditFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.model = ToDoRepository.find(args.modelId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.actions_edit, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.save -> {
+                save(); return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun save() {
+        val edited = binding.model?.copy(
+            description = binding.desc.text.toString(),
+            isCompleted = binding.isCompleted.isChecked,
+            notes = binding.notes.text.toString()
+        )
+        edited?.let { ToDoRepository.save(it) }
+        navToDisplay()
+    }
+
+    private fun navToDisplay() {
+        hideKeyboard()
+        findNavController().popBackStack()
+    }
+
+    // from https://stackoverflow.com/questions/21573586/hidesoftinputfromwindow-not-working/21574135#21574135
+    private fun hideKeyboard() {
+        view?.let {
+            val imm = context?.getSystemService<InputMethodManager>()
+            imm?.hideSoftInputFromWindow(
+                it.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+        }
     }
 }
